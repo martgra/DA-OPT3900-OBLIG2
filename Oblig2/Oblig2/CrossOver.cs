1d2836d1ce9ad119bac6fdcfc3857291c0c8748b
Martin Gran <martgra@gmail.com>
2016-10-26 12:36:30 +0200

    Opprettet klasser

---
+++ Oblig2/Oblig2/CrossOver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oblig2
+{
+    class CrossOver
+    {
+        public static List<int []> TwoPointCrossover(int [,] mates)
+        {
+            List<int[]> newGeneration = new List<int[]>();
+
+            return newGeneration;
+        }
+    }
+}
